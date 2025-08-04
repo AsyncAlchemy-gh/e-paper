b964abc121c97af70025c1830269a48c346d0757
AsyncAlchemy-gh <async.alchemy@gmail.com>
2025-08-03 23:32:54 -0500

    fix font path

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import logging
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
-picdir = os.path.join(os.getcwd(), 'pic')
+# picdir = os.path.join(os.getcwd(), 'pic')
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -20,28 +20,28 @@ try:
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    font40 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
+    font24 = ImageFont.truetype('Font.ttc', 24)
+    font18 = ImageFont.truetype('Font.ttc', 18)
+    font40 = ImageFont.truetype('Font.ttc', 40)
     
     # Drawing on the image
-    logging.info("1.Drawing on the image...")
-    Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
-    draw = ImageDraw.Draw(Himage)
-    draw.text((5, 0), 'hello world', font = font40, fill = epd.BLUE)
+    # logging.info("1.Drawing on the image...")
+    # Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
+    # draw = ImageDraw.Draw(Himage)
+    # draw.text((5, 0), 'hello world', font = font40, fill = epd.BLUE)
 
-    draw.line((5, 170, 80, 245), fill = epd.BLUE)
-    draw.line((80, 170, 5, 245), fill = epd.ORANGE)
-    draw.rectangle((5, 170, 80, 245), outline = epd.BLACK)
-    draw.rectangle((90, 170, 165, 245), fill = epd.GREEN)
-    draw.arc((5, 250, 80, 325), 0, 360, fill = epd.RED)
-    draw.chord((90, 250, 165, 325), 0, 360, fill = epd.YELLOW)
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(3)
+    # draw.line((5, 170, 80, 245), fill = epd.BLUE)
+    # draw.line((80, 170, 5, 245), fill = epd.ORANGE)
+    # draw.rectangle((5, 170, 80, 245), outline = epd.BLACK)
+    # draw.rectangle((90, 170, 165, 245), fill = epd.GREEN)
+    # draw.arc((5, 250, 80, 325), 0, 360, fill = epd.RED)
+    # draw.chord((90, 250, 165, 325), 0, 360, fill = epd.YELLOW)
+    # epd.display(epd.getbuffer(Himage))
+    # time.sleep(3)
     
     # read bmp file 
     logging.info("2.read bmp file")
-    Himage = Image.open(os.path.join(picdir, 'logo.bmp'))
+    Himage = Image.open('logo.bmp')
     epd.display(epd.getbuffer(Himage))
     time.sleep(3)
 
